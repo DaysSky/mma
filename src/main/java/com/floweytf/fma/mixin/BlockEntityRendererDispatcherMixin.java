dffc51037bc62daf230ff6137e4a0de5607bed72
Jade <infernoblaze326@gmail.com>
2025-03-15 15:21:01 +0800

    """"""Chest Waypoints""""""""

---
+++ src/main/java/com/floweytf/fma/mixin/BlockEntityRendererDispatcherMixin.java
@@ -0,0 +1,8 @@
+package com.floweytf.fma.mixin;
+
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BlockEntityRenderDispatcher.class)
+public class BlockEntityRendererDispatcherMixin {
+}
